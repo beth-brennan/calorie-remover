34b41233f85e4005085e7a401e1f2d0ac5b8de89
Beth Brennan <bethbrennan2@gmail.com>
2022-09-12 19:14:10 -0400

    hides calorie labels functionality

---
+++ calorieRemover.js
@@ -0,0 +1,5 @@
+const elements = document.getElementsByClassName("ccl-649204f2a8e630fd ccl-6f43f9bb8ff2d712 ccl-32ec9a3197735a65 ccl-08c109442f3e666d ccl-40ad99f7b47f3781 ccl-ea9144c387bfb5b8");
+
+for (const element of elements) {
+  element.innerHTML = "";
+}
