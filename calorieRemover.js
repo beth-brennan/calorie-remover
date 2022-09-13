a7a7d932cb8c2aeb9adab8433039a77dc8e7297d
Beth Brennan <bethbrennan2@gmail.com>
2022-09-13 12:56:51 -0400

    removes calorie labels from cart

--- calorieRemover.js
+++ calorieRemover.js
@@ -1,6 +1,6 @@
 // Full Menu
 
-const menuElements = document.querySelectorAll("p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-32ec9a3197735a65.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781.ccl-ea9144c387bfb5b8");
+const menuElements = document.querySelectorAll("p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-32ec9a3197735a65.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781.ccl-ea9144c387bfb5b8, p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781");
 
 for (const element of menuElements) {
   element.innerHTML = "";
@@ -8,16 +8,36 @@ for (const element of menuElements) {
 
 // Individual Item Modal
 
-const targetNode = document.querySelector("body");
+const itemNode = document.querySelector("body");
 
-const config = { childList: true };
+const itemConfig = { childList: true, attributes: true, characterData: true, subtree: true };
 
-const callback = (mutationList, observer) => {
+const itemCallback = (mutationList, observer) => {
   for (const mutation of mutationList) {
-    if (mutation.type === 'childList') {
-      document.querySelector(".MenuItemModal-64049887e657bc38").remove();
+    document.querySelector(".MenuItemModal-64049887e657bc38").remove();
 
-      const addons = document.querySelectorAll("p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781");
+    const addons = document.querySelectorAll("p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781, p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-32ec9a3197735a65.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781.ccl-ea9144c387bfb5b8");
+
+    for (const element of addons) {
+      element.innerHTML = "";
+    }
+  }
+}
+
+const itemObserver = new MutationObserver(itemCallback);
+
+itemObserver.observe(itemNode, itemConfig);
+
+// Cart Section
+
+const cartNode = document.querySelector(".Carousel-7ebf786c2374ca6b");
+
+const cartConfig = { childList: true, attributes: true };
+
+const cartCallback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === 'childList' || mutation.type === 'attributes') {
+      const addons = document.querySelectorAll("p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781, p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-32ec9a3197735a65.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781.ccl-ea9144c387bfb5b8");
 
       for (const element of addons) {
         element.innerHTML = "";
@@ -26,6 +46,28 @@ const callback = (mutationList, observer) => {
   }
 }
 
-const observer = new MutationObserver(callback);
+const cartObserver = new MutationObserver(cartCallback);
 
-observer.observe(targetNode, config);
+cartObserver.observe(cartNode, cartConfig);
+
+// Modal Changes
+
+const modalNode = document.querySelector(".ccl-9237ff225b0d2789.ccl-e770a464a394226e.ccl-bae50209245d2304");
+
+const modalConfig = { childList: true, attributes: true };
+
+const modalCallback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === 'childList' || mutation.type === 'attributes') {
+      const addons = document.querySelectorAll("p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781, p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-32ec9a3197735a65.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781.ccl-ea9144c387bfb5b8");
+
+      for (const element of addons) {
+        element.innerHTML = "";
+      }
+    }
+  }
+}
+
+const modalObserver = new MutationObserver(modalCallback);
+
+modalObserver.observe(modalNode, modalConfig);
