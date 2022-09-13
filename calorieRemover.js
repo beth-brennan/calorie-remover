cd3a6889178fe86bc3736f42e836807f7d4346be
Beth Brennan <bethbrennan2@gmail.com>
2022-09-12 22:03:17 -0400

    hides calorie labels when adding items to cart

--- calorieRemover.js
+++ calorieRemover.js
@@ -1,5 +1,31 @@
-const elements = document.getElementsByClassName("ccl-649204f2a8e630fd ccl-6f43f9bb8ff2d712 ccl-32ec9a3197735a65 ccl-08c109442f3e666d ccl-40ad99f7b47f3781 ccl-ea9144c387bfb5b8");
+// Full Menu
 
-for (const element of elements) {
+const menuElements = document.querySelectorAll("p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-32ec9a3197735a65.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781.ccl-ea9144c387bfb5b8");
+
+for (const element of menuElements) {
   element.innerHTML = "";
 }
+
+// Individual Item Modal
+
+const targetNode = document.querySelector("body");
+
+const config = { childList: true };
+
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === 'childList') {
+      document.querySelector(".MenuItemModal-64049887e657bc38").remove();
+
+      const addons = document.querySelectorAll("p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781");
+
+      for (const element of addons) {
+        element.innerHTML = "";
+      }
+    }
+  }
+}
+
+const observer = new MutationObserver(callback);
+
+observer.observe(targetNode, config);
