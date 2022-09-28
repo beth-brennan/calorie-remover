1c1c0c6253809ffa24c0145f1ee566a2493689a3
Beth Brennan <bethbrennan2@gmail.com>
2022-09-28 15:57:01 -0400

    bug fix

--- calorieRemover.js
+++ calorieRemover.js
@@ -10,7 +10,7 @@ for (const element of menuElements) {
 
 const body = document.querySelector("body");
 
-const config = { attributes: true };
+const config = { attributes: true, subtree: true };
 
 const callback = (mutationList, observer) => {
   for (const mutation of mutationList) {
@@ -18,10 +18,11 @@ const callback = (mutationList, observer) => {
       const addons = document.querySelectorAll("p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781, p.ccl-649204f2a8e630fd.ccl-6f43f9bb8ff2d712.ccl-32ec9a3197735a65.ccl-08c109442f3e666d.ccl-40ad99f7b47f3781.ccl-ea9144c387bfb5b8");
 
       for (const element of addons) {
-        element.remove();
+        element.innerHTML = "";
       }
 
       document.querySelector(".MenuItemModal-64049887e657bc38").remove();
+
     }
   }
 }
@@ -29,4 +30,3 @@ const callback = (mutationList, observer) => {
 const observer = new MutationObserver(callback);
 
 observer.observe(body, config);
-
